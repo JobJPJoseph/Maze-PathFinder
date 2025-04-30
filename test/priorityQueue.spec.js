b093be204438cb7b270650de75b6dd822923cfc6
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2025-04-30 03:48:55 -0400

    completed bubbleSort(), binarySortUp(), and enqueue()

--- test/priorityQueue.spec.js
+++ test/priorityQueue.spec.js
@@ -1,18 +1,21 @@
-const { MaxPriorityQueue } = require(`../lib/priority-queue`);
+const { MinPriorityQueue } = require(`../lib/priority-queue`);
 
 const chai = require('chai');
+const spies = require('chai-spies');
 const expect = chai.expect;
 
-describe('MaximumPriorityQueue', function () {
+chai.use(spies);
 
-    it('should initialize a class called MaxPriorityQueue', function () {
-        expect(MaxPriorityQueue).to.exist;
+describe('MinimumPriorityQueue', function () {
+
+    it('should initialize a class called MinPriorityQueue', function () {
+        expect(MinPriorityQueue).to.exist;
     });
 
     let mpQueue;
 
     beforeEach(function () {
-        mpQueue = new MaxPriorityQueue();
+        mpQueue = new MinPriorityQueue();
     });
 
     describe('constructor', function () {
@@ -23,24 +26,30 @@ describe('MaximumPriorityQueue', function () {
             expect(mpQueue.heap.length).to.equal(0);
         });
 
+        it('should create a property called this.length that represent the length of this.heap', function () {
+            expect(mpQueue.length).to.exist;
+            expect(mpQueue.length).to.be.a('number');
+            expect(mpQueue.length).to.equal(0);
+        });
+
     });
 
     describe('getLeftChildIndex', function () {
 
-        it('should return the result of ((2 * i) - 1)', function () {
-            expect(mpQueue.getLeftChildIndex(5)).to.equal(9);
-            expect(mpQueue.getLeftChildIndex(2)).to.equal(3);
-            expect(mpQueue.getLeftChildIndex(3)).to.equal(5);
+        it('should return the result of ((2 * i) + 1)', function () {
+            expect(mpQueue.getLeftChildIndex(5)).to.equal(11);
+            expect(mpQueue.getLeftChildIndex(2)).to.equal(5);
+            expect(mpQueue.getLeftChildIndex(3)).to.equal(7);
         });
 
     });
 
     describe('getRightChildIndex', function () {
 
-        it('should return the result of ((2 * i) - 2)', function () {
-            expect(mpQueue.getRightChildIndex(1)).to.equal(0);
-            expect(mpQueue.getRightChildIndex(2)).to.equal(2);
-            expect(mpQueue.getRightChildIndex(4)).to.equal(6);
+        it('should return the result of ((2 * i) + 2)', function () {
+            expect(mpQueue.getRightChildIndex(1)).to.equal(4);
+            expect(mpQueue.getRightChildIndex(2)).to.equal(6);
+            expect(mpQueue.getRightChildIndex(4)).to.equal(10);
         });
 
     });
@@ -53,6 +62,111 @@ describe('MaximumPriorityQueue', function () {
             expect(mpQueue.getParentIndex(2)).to.equal(0);
 
         });
+
+    });
+
+    describe('bubbleSort', function () {
+
+        it('should switch the values that are referenced', function () {
+            let obj = { value: "S", priority: 25};
+            let obj1 = { value: "E", priority: 23 };
+
+            mpQueue.heap.push(obj, obj1);
+
+            expect(mpQueue.heap[0].value).to.equal("S");
+            expect(mpQueue.heap[1].value).to.equal('E');
+
+            mpQueue.bubbleSort(0, 1);
+
+            expect(mpQueue.heap[0].value).to.equal("E");
+            expect(mpQueue.heap[1].value).to.equal('S');
+        });
+
+    });
+
+    // Priority represents the distance from the node to the exit
+    describe('binarySortUp', function () {
+
+        // Does not need to accept an object. Assume last index
+        it('it should sort the object based on its priority', function () {
+            let obj = { value: "S", priority: 25};
+            mpQueue.heap.push(obj);
+            mpQueue.length++;
+
+            // check properties
+            expect(mpQueue.heap[0]).to.be.a('object');
+            expect(mpQueue.heap[0].value).to.exist.and.to.equal("S");
+            expect(mpQueue.heap[0].priority).to.exist.and.to.equal(25);
+
+            // check its position
+            let obj1 = { value: "E", priority: 23 };
+            mpQueue.heap.push(obj1);
+            mpQueue.length++;
+
+            mpQueue.binarySortUp();
+
+            expect(mpQueue.heap[0].value).to.equal("E");
+            expect(mpQueue.heap[1].value).to.equal("S");
+        });
+
+    });
+
+    describe('enqueue', function () {
+
+        it('should accept two arguments', function () {
+            let spyEnqueue = chai.spy.on(mpQueue, 'enqueue');
+            mpQueue.enqueue("test", 24);
+            expect(spyEnqueue).to.be.called.with('test', 24);
+            chai.spy.restore(mpQueue, 'enqueue');
+        });
+
+        it('should call binarySortUp', function () {
+            let spySortUp = chai.spy.on(mpQueue, 'binarySortUp');
+            mpQueue.enqueue("test", 24);
+            expect(spySortUp).to.be.called.once;
+            chai.spy.restore(mpQueue, 'binarySortUp');
+        });
+
+
+        it('should add the value into the queue', function () {
+            // let obj = { value: "S", priority: 25};
+            let val = "S";
+            let distance = 25;
+            mpQueue.enqueue(val, distance);
+
+            expect(mpQueue.heap[0]).to.be.a('object');
+            expect(mpQueue.heap[0].value).to.exist.and.to.equal("S");
+            expect(mpQueue.heap[0].priority).to.exist.and.to.equal(25);
+        });
+
+        it('should increment the length', function () {
+            let val = "S";
+            let distance = 25;
+            mpQueue.enqueue(val, distance);
+
+            expect(mpQueue.length).to.equal(1);
+        });
+
+        it('should the sort the heap', function () {
+            let val = "S";
+            let distance = 25;
+            mpQueue.enqueue(val, distance);
+
+            let val2 = "E";
+            let distance2 = 24;
+            mpQueue.enqueue(val2, distance2);
+
+            expect(mpQueue.heap[0].value).to.equal('E');
+            expect(mpQueue.heap[1].value).to.equal('S');
+        });
+
+    });
+
+    describe('binarySortDown', function () {
+
+    });
+
+    describe('dequeue', function () {
 
     });
 
