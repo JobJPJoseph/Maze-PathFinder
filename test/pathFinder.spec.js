0162210e7a32bb7d30f3aca0e884ccbced3513d8
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2025-05-03 04:43:31 -0400

    setting up the tests for the pathfinder

---
+++ test/pathFinder.spec.js
@@ -0,0 +1,36 @@
+const { PathFinder } = require('../lib/pathFinder');
+
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('PathFinder Project', function () {
+
+    it('', function () {
+
+    });
+
+    describe('getNeighbors', function () {
+
+        it('should declare the function', function () {
+
+        });
+
+        it('should return valid coordinate of the targets neighbors', function () {
+
+        });
+
+    });
+
+    describe('getPath', function () {
+
+        it('should declare the function', function () {
+
+        });
+
+        it('should return the optimal path from S to E', function () {
+
+        });
+
+    });
+
+});
